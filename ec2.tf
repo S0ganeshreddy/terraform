257a018afeddd44cfae321a558a711d2f22f91da
S0ganeshreddy <80666975+S0ganeshreddy@users.noreply.github.com>
2023-05-16 15:17:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "app" {
   subnet_id              = aws_subnet.pub-sub.id
   availability_zone      = "us-east-1a"
   key_name               = "intern"
-  user_data              = file("httpd.sh")
+  #user_data              = file("httpd.sh")
   vpc_security_group_ids = [aws_security_group.ssh-http.id]
   tags = {
     Name = "APP"
@@ -19,7 +19,7 @@ resource "aws_instance" "app1" {
   subnet_id              = aws_subnet.pub-sub1.id
   availability_zone      = "us-east-1b"
   key_name               = "intern"
-  user_data              = file("httpd.sh")
+  #user_data              = file("httpd.sh")
   vpc_security_group_ids = [aws_security_group.ssh-http.id]
   tags = {
     Name = "APP1"
